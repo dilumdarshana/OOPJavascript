91dff5166d51a224dcca6b0e9314daa7e763b355
Dilum Darshana <dilum.dar@gmail.com>
2016-09-07 23:25:16 +0530

    Few changes

--- src/app.js
+++ src/app.js
@@ -2,5 +2,7 @@ import {Human} from './classes/human.js';
 import {Bird} from './classes/bird.js';
 
 let human1 = new Human('Apache', 34);
+let human2 = new Human('Tom', 35);
 
 console.log(human1);
+console.log(human2);
